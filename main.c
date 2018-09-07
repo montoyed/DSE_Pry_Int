bcaf2aede3700cf1cb66599703534db15184b379
montoyed <edgar.montoya.dse@uteq.edu.mx>
2018-09-07 12:12:13 -0500

    Added function division

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 #include <stdio.h>
 #include <math.h>
+#define RESULTADO 5
 
+float division(float a, float b);
 int una_suma(int a, int b);
 
 int main()
 {
     printf("Hola mundo\n");
+    division();
     return 0;
 }
